c17910de5a0b6e0a7858aea3db33ea17e01f3b36
İdris Quluzada <idrisquluzade07@gmail.com>
2026-04-17 16:42:19 +0400

    all

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
         res = a / b
-    except ZeroDivisionError:
+    except Exception:
         return None
     finally:
         print("Inside result: {}".format(res))
