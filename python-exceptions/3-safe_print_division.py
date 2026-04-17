bccd62abb496b9b05c1566ed49723ab818fa27c9
İdris Quluzada <idrisquluzade07@gmail.com>
2026-04-17 16:39:50 +0400

    all

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print(f"Inside result:{res}")
+        print("Inside result: {}".format(res))
         return res
--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,25 +2,16 @@
 def list_division(my_list_1, my_list_2, list_length):
     res_list = []
     for i in range(list_length):
-
         res = 0
-
         try:
             val1, val2 = my_list_1[i], my_list_2[i]
             res = val1/val2
-
         except TypeError:
             print("wrong type")
-            
-
         except ZeroDivisionError:
             print("division by 0")
-            
-
         except IndexError:
             print("out of range")
-            
         finally:
             res_list.append(res)
-
     return res_list
