6fef9a4629884434c555faa4a72587d5cb3b720b
İdris Quluzada <idrisquluzade07@gmail.com>
2026-04-25 18:18:23 +0400

    edit

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -15,17 +15,43 @@ def about():
 def contact():
     return render_template('contact.html')
 
-@app.route('/items')
-def items():
-    try:
-        with open('items.json', 'r') as file:
-            data = json.load(file)
-            items_list = data.get("items", [])
-    except Exception as e:
-        print(f"Error reading JSON: {e}")
-        items_list = []
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
 
-    return render_template('items.html', items=items_list)
+    if source == 'json':
+        data = load_json()
+    elif source == 'csv':
+        data = load_csv()
+    elif source == 'sql':
+        data = load_sqlite()
+    else:
+        return render_template('product_display.html',
+                               error="Wrong source")
+
+    if data is None:
+        return render_template('product_display.html',
+                               error="Error loading data")
+
+    # ✅ HANDLE ID FILTERING
+    if product_id:
+        try:
+            product_id = int(product_id)
+            filtered = [p for p in data if p["id"] == product_id]
+
+            if not filtered:
+                return render_template('product_display.html',
+                                       error="Product not found")
+
+            data = filtered
+
+        except ValueError:
+            return render_template('product_display.html',
+                                   error="Product not found")
+
+    return render_template('product_display.html',
+                           products=data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
